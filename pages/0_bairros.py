7777f8204f506b75ec7f2c2d4d470f7f1b8968f4
Cleristomsilva <124253169+Cleristomsilva@users.noreply.github.com>
2024-05-10 19:25:32 -0300

    Update 0_bairros.py

--- pages/0_bairros.py
+++ pages/0_bairros.py
@@ -6,65 +6,101 @@ import plotly.graph_objects as go
 
 def conectar_banco_de_dados():
     # Conectar ao banco de dados MySQL
-    try:
-        conn = mysql.connector.connect(
-            host="localhost",
-            port=3306,
-            user="root",
-            password="",
-            database="pesquisa"
-        )
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM pesquisa")
-        dados = cursor.fetchall()
-        return dados
-    except mysql.connector.Error as error:
-        print("Erro ao conectar ao banco de dados: {}".format(error))
+    conn_mysql = mysql.connector.connect(
+        host="189.17.195.66",
+        port=41306,
+        user="app",
+        password="AppP3squIs@",
+        database="app"
+    )
+    cursor_mysql = conn_mysql.cursor()
+
+    # Recuperar dados da pesquisa
+    cursor_mysql.execute("SELECT * FROM pesquisa")
+    dados = cursor_mysql.fetchall()
+
+    # Fechar conexão
+    conn_mysql.close()
+
+    return dados
 
 
-def criar_grafico_barras(dados, candidatos_selecionados):
+def grafico_bairros(dados, candidatos_selecionados, figsize=(12, 5)):
     df = pd.DataFrame(dados)
     df_selected = df[df[12].isin(candidatos_selecionados)]
-    bairros = df_selected[4].value_counts().sort_index()
+    bairros_counts = df_selected[4].value_counts().sort_index()
 
-    fig = go.Figure(data=[go.Bar(x=bairros.index, y=bairros.values, text=bairros.values, textposition='auto')])
-    fig.update_layout(
+    # Criar um DataFrame com os dados dos bairros e o número de votos de cada candidato
+    bairros_info = pd.DataFrame(index=bairros_counts.index)
+    for candidato in candidatos_selecionados:
+        bairros_info[candidato] = df_selected[df_selected[12] == candidato][4].value_counts()
+
+    # Criar lista de barras para o gráfico Plotly
+    bar_data = []
+    for candidato in candidatos_selecionados:
+        bar_data.append(go.Bar(
+            x=bairros_info.index,
+            y=bairros_info[candidato],
+            name=candidato,
+            hoverinfo='y+name'
+        ))
+
+    # Layout do gráfico
+    layout = go.Layout(
         title='Distribuição por Bairros',
-        xaxis_title='Bairros',
-        yaxis_title='Número de Participantes',
+        xaxis=dict(title='Bairros'),
+        yaxis=dict(title='Número de Participantes'),
+        barmode='stack'
     )
+
+    # Criar figura Plotly
+    fig = go.Figure(data=bar_data, layout=layout)
 
     return fig
 
 
 def main():
-    st.set_page_config(
-        page_title="Análise da Pesquisa de Intenção de Votos",
-        page_icon=":bar_chart:",
-        layout="wide",
-        initial_sidebar_state="expanded"
-    )
-    st.title('Análise da Pesquisa de Intenção de Votos')
-
-    dados = conectar_banco_de_dados()
-
-    candidatos = ['Luciana e Kadu', 'Dr. Paulo e Guará', 'Dr. Rafael e Juninho', 'Dr. Márcio e Magali']
-    candidatos_selecionados = st.sidebar.multiselect('Selecione os Candidatos:', candidatos, default=candidatos)
-
-    if not dados:
+    # se houver conexão com o banco mostra os graficos se nao houver mostra a mensagem sem conexão
+    if not conectar_banco_de_dados():
         st.error('Sem conexão com o banco de dados.')
         return
+    else:
+        st.set_page_config(
+            page_title="Análise da Pesquisa de Intenção de Votos",
+            page_icon=":bar_chart:",
+            layout="wide",  # Layout amplo para usar todo o espaço horizontal
+            initial_sidebar_state="expanded"  # Barra lateral inicialmente expandida
+        )
+        st.title('Análise da Pesquisa de Intenção de Votos')
+        # tamanho da fonte do title
+        st.markdown("""<style>.title {font-size:16px;}</style>""", unsafe_allow_html=True)
 
-    total_participantes = 0
-    for candidato in candidatos_selecionados:
-        num_votos = len([dado for dado in dados if dado[12] == candidato])
-        st.sidebar.write(f'**{candidato}= {num_votos}**',)
-        total_participantes += num_votos
+        # Conectar ao banco de dados e recuperar os dados da pesquisa
+        dados = conectar_banco_de_dados()
 
-    st.sidebar.write(f'**Total de Participantes:** {total_participantes}')
+        # Opções de candidatos
+        candidatos = ['Luciana e Kadu', 'Dr. Paulo e Guará', 'Dr. Rafael e Juninho', 'Dr. Márcio e Magali']
 
-    fig_bairros = criar_grafico_barras(dados, candidatos_selecionados)
-    st.plotly_chart(fig_bairros)
+        # # Filtro na barra lateral para selecionar os candidatos
+        candidatos_selecionados = st.sidebar.multiselect('Selecione os Candidatos:', candidatos, default=candidatos)
+
+        # Filtro na barra lateral para selecionar o total de participantes titulo em negrito e fonte maior
+        st.sidebar.markdown('**Total Apurado:**', unsafe_allow_html=True)
+
+        # Filtro na barra lateral para selecionar o total de participantes
+        if len(candidatos_selecionados) > 0:
+            total_participantes = 0
+            for candidato in candidatos_selecionados:
+                num_votos = len([dado for dado in dados if dado[12] == candidato])
+                st.sidebar.write(f'**{candidato}= {num_votos}**', )
+                # st.sidebar.write(f'**Total de Votos:** {num_votos}')
+                total_participantes += num_votos
+
+            st.sidebar.write(f'**Total de Participantes:** {total_participantes}')
+
+        # Criar gráfico de bairros interativo com Plotly
+        fig_bairros = grafico_bairros(dados, candidatos_selecionados)
+        st.plotly_chart(fig_bairros)
 
 
 if __name__ == "__main__":
